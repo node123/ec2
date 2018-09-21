6940ee686f8cd7feec5569aa68c08d876146e312
Devops <7wc.info@gmail.com>
2018-09-21 16:08:19 +0530

    updated ec2 files

--- main.tf
+++ main.tf
@@ -28,3 +28,17 @@ resource "aws_instance" "web" {
     Name = "HelloWorld"
   }
 }
+
+#AWS Config
+
+variable "aws_access_key" {
+  default = "AKIATQAVOB6BPMMSW37U"
+}
+
+variable "aws_secret_key" {
+  default = "0b6m6uc0IHi0FwuxSfWV99DGSk3voS399qijjYZM"
+}
+
+variable "aws_region" {
+  default = "us-east-1"
+
